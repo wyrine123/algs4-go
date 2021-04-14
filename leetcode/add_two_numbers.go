c936cc1b0a17f3282de7bb941a7fcb217e7c51e7
wyrine <panda429844881@gmail.com>
2021-04-15 00:42:05 +0800

    leetcode:两数相加 优化, 使用原链表保存结果。 todo: 看看代码怎么优化下，有点不太好懂

--- leetcode/add_two_numbers.go
+++ leetcode/add_two_numbers.go
@@ -10,31 +10,33 @@ import "fmt"
 
 // todo 结果直接使用l1
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	result := new(ListNode)
 	if l1 == nil || l2 == nil {
-		return result
+		return nil
 	}
 
 	carry := 0 // 进位
 
-	result.Val = -1
-	resultNode := result
-
-	for l1Node, l2Node := l1, l2; l1Node != nil || l2Node != nil || carry != 0; {
+	// 使用l1保存结果
+	// 这儿循环内保证 l1一定比l2长,所以就不判断 l2 != nil 这个条件了
+	for l1Node, l2Node := l1, l2; l1Node != nil || carry != 0; {
 		sum := 0
-		node := new(ListNode)
 		if l1Node != nil && l2Node != nil {
 			sum = l1Node.Val + l2Node.Val + carry
-			if sum < 10 {
-				carry = 0
-				node.Val = sum
-			} else {
+			if sum >= 10 {
 				// 需要进位
 				carry = 1
-				node.Val = sum - 10
+				sum -= 10
+			} else {
+				carry = 0
 			}
-			resultNode.Next = node
-			resultNode = node
+			if (l1Node.Next == nil && l2Node.Next != nil) || (l1Node.Next == nil && carry != 0) {
+				// l2比l1链表长,或者链表结束,但是carry为不为0 需要给l1新增一个进位节点
+				zeroNode := new(ListNode)
+				zeroNode.Val = carry
+				l1Node.Next = zeroNode
+				carry = 0 // 清除进位
+			}
+			l1Node.Val = sum // 原地保存
 
 			l1Node = l1Node.Next
 			l2Node = l2Node.Next
@@ -43,65 +45,45 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		if l1Node != nil && l2Node == nil {
 			// l1不为空，l2为空
 			sum = l1Node.Val + carry
-			if sum < 10 {
-				carry = 0
-				node.Val = sum
-			} else {
+			if sum >= 10 {
 				// 需要进位
 				carry = 1
-				node.Val = sum - 10
+				sum -= 10
+			} else {
+				carry = 0
 			}
-			resultNode.Next = node
-			resultNode = node
-
+			if l1Node.Next == nil && carry != 0 {
+				// l1结束,但是还有进位
+				zeroNode := new(ListNode)
+				zeroNode.Val = carry
+				l1Node.Next = zeroNode
+				carry = 0 // 清除进位
+			}
+			l1Node.Val = sum // 原地保存
 			l1Node = l1Node.Next
 			continue
 		}
-		if l2Node != nil && l1Node == nil {
-			// l2不为空,l1为空
-			sum = l2Node.Val + carry
-			if sum < 10 {
-				carry = 0
-				node.Val = sum
-			} else {
-				// 需要进位
-				carry = 1
-				node.Val = sum - 10
-			}
-			resultNode.Next = node
-			resultNode = node
-
-			l2Node = l2Node.Next
-			continue
-		}
-		// l1,l2都为空,但carry!=0
-		// 新增一个node
-		node.Val = carry
-		resultNode.Next = node
-		carry = 0
 	}
 
-	// 删除第一个节点
-	result = result.Next
-	return result
+	return l1
 }
 
 func main() {
 	l1 := new(ListNode)
-	l1.Val = 2
+	l1.Val = 9
 	l1.Next = new(ListNode)
-	l1.Next.Val = 4
+	l1.Next.Val = 9
 	l1.Next.Next = new(ListNode)
-	l1.Next.Next.Val = 3
+	l1.Next.Next.Val = 9
 
 	l2 := new(ListNode)
-	l2.Val = 5
-	l2.Next = new(ListNode)
-	l2.Next.Val = 6
-	l2.Next.Next = new(ListNode)
-	l2.Next.Next.Val = 7
+	l2.Val = 9
+	//l2.Next = new(ListNode)
+	//l2.Next.Val = 6
+	//l2.Next.Next = new(ListNode)
+	//l2.Next.Next.Val = 7
 
-	result := addTwoNumbers(l1, l2)
+	result := addTwoNumbers(l2, l1)
 	for node := result; node != nil; node = node.Next {
 		fmt.Printf("%d\t", node.Val)
 	}
