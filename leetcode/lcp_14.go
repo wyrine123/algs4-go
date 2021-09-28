a0cca0a6c1db647e34003f75d6097374f8e37363
wyrine <panda429844881@gmail.com>
2021-09-28 15:25:32 +0800

    leetcode 14: 最长公共前缀 优化下内存空间使用

--- leetcode/lcp_14.go
+++ leetcode/lcp_14.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
  14. 最长公共前缀
 	https://leetcode-cn.com/problems/longest-common-prefix/
@@ -11,6 +13,8 @@ func longestCommonPrefix(strs []string) string {
 	}
 
 	var prefix = []byte(strs[0]) // 默认最长公共前缀就是数组下标0
+	// 为了节约内存空间 每次不再重新生成一个prefix,而是共用一个数组,通过一个下标来标识当前
+	var prefixIndex = len(prefix)
 
 	for i := 1; i < len(strs); i++ {
 		// 寻找prefix和当前字符串的最长公共前缀
@@ -22,31 +26,31 @@ func longestCommonPrefix(strs []string) string {
 
 		// 获取current和prefix中短的那个数组长度
 		var min int
-		if len(prefix) <= len(current) {
-			min = len(prefix)
+		if prefixIndex <= len(current) {
+			min = prefixIndex
 		} else {
 			min = len(current)
 		}
 
-		var currentPrefix []byte
+		// prefixIndex 重置为0
+		prefixIndex = 0
 		for j := 0; j < min; j++ {
 			if prefix[j] == current[j] {
-				currentPrefix = append(currentPrefix, prefix[j])
+				prefixIndex++
 			} else {
 				break
 			}
 		}
 
-		if len(currentPrefix) == 0 {
+		if prefixIndex == 0 {
 			return ""
-		} else {
-			prefix = currentPrefix
 		}
 	}
 
-	return string(prefix)
+	return string(prefix[:prefixIndex])
 }
 
-//func main() {
-//	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
-//}
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "f"}))
+
+}
